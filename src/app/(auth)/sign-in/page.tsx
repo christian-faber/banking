b47efa97edeacaa0568d1632eb832a43b6161083
Christian <christian.a.faber@gmail.com>
2024-09-13 11:36:57 -1000

    first commit

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SignIn: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to JSM Banking</h1>
+      <p>This is the sign in page</p>
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to JSM Banking</h1>
+      <p>This is the root Home of your application.</p>
+    </div>
+  );
+};
+
+export default Home;
